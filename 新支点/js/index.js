d79c8d5c81f40c8eaf2c9bd779d0a842efe12753
11 <2038589669a>
2021-02-01 15:45:25 +0800

    22

---
+++ 新支点/js/index.js
@@ -0,0 +1,19 @@
+$(window).scroll(function() {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    scrollTop > 52 ? $(".header_nav").addClass("navbar-fixed-top"):$(".header_nav").removeClass("navbar-fixed-top");
+    $(".banner_select").css({backgroundPosition: `0% ${scrollTop}px`});
+    if (scrollTop>650) {
+        let x;
+        x = (640-scrollTop)/2;
+        $(".section_box3").css({backgroundPosition: `0% ${x}px`})
+    }
+    if (scrollTop>1450) {
+        let x;
+        x = (1550-scrollTop)/2.5;
+        $(".section_bg").css({backgroundPosition: `0% ${x}px`})
+    }
+  })
+  
+  if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
+    new WOW().init();
+};
